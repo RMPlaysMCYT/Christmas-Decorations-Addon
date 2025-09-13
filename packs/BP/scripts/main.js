4b8a0fda0deeda8b95eccb247fd7de7c1f0140c2
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2025-09-13 16:57:05 +0800

    Add custom interaction for blue gift block in JSON and update registration in main.js

--- packs/BP/scripts/main.js
+++ packs/BP/scripts/main.js
@@ -1,7 +1,7 @@
 import {world, system} from "@minecraft/server";
 
 world.beforeEvents.worldInitialize.subscribe((event) => {
-    event.blockComponentRegistry.registeCustomrComponent("christmas_pack:christmas_tree", {
+    event.blockComponentRegistry.registeCustomrComponent("christmas_pack:gift_interact", {
         onPlayerInteract: event => {
             const {player, block} = event;
             player.runCommand('say Wow you touch the gift')
