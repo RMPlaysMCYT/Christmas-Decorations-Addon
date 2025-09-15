3d40efcd36eb02832f8b850c50dbf9e559032c11
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2025-09-15 09:53:16 +0800

    Refactor onPlayerInteract event and add loot table for gift interactions

--- packs/BP/scripts/main.js
+++ packs/BP/scripts/main.js
@@ -8,18 +8,23 @@ import { GreenGift, OrangeGift } from "./blocks/gift";
 world.beforeEvents.worldInitialize.subscribe(event => {
     event.blockComponentRegistry.registerCustomComponent("christmas_pack:gift_interact", {
         onPlayerInteract: event => {
-            const {player, block} = event;
-            player.runCommand("say Hi");
-        },
-        onStepOn: event => {
-            const { block, dimension, player } = event;
-            const blockLocationes = block.location;
-            const blockLocationes2 = `${blockLocationes.x} ${blockLocationes.y} ${blockLocationes.z}`;
-            player.runCommand(`say Block Location: ${blockLocationes2}`);
+            const {player, block, dimension} = event;
+            const {x, y, z} = block.center();
+            const blockLocationes = `${x} ${y} ${z}`;
+            event.dimension.runCommand("say Suprise!");
+            dimension.runCommand(`loot spawn ${blockLocationes} loot "gift1"`);
         }
+        // ,
+        // onStepOn: event => {
+        //     const { block, player, entity } = event;
+        //     const blockLocationes = block.location;
+        //     const blockLocationes2 = `${blockLocationes.x} ${blockLocationes.y} ${blockLocationes.z}`;
+        //     entity.runCommand("say Block Location:" + blockLocationes2);
+        // }
     })
     event.blockComponentRegistry.registerCustomComponent("christmas_pack:gift_green_interact", new GreenGift());
     event.blockComponentRegistry.registerCustomComponent("christmas_pack:gift_orange_interact", new OrangeGift());
 });
 
 console.log("Christmas Pack Loaded");
+console.warn("Christmas Pack Loaded");
