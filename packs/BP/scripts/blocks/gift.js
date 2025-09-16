926cc1e898fcb58d7c45d63cdf7ad1b694d4dc2d
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2025-09-16 10:33:39 +0800

    Refactor gift interaction logic and add loot tables for GreenGift and OrangeGift

--- packs/BP/scripts/blocks/gift.js
+++ packs/BP/scripts/blocks/gift.js
@@ -1,13 +1,21 @@
 export class GreenGift {
     onPlayerInteract(event){
         const {player, block, dimension, entity} = event;
-        player.runCommand("say Hi");
+        const {x, y, z} = block.center();
+        const blockLocationes = `${x} ${y} ${z}`;
+        player.runCommand("say Suprise!!");
+        dimension.runCommand(`loot spawn ${blockLocationes} loot "gift2"`);
+        dimension.runCommand(`setblock ${blockLocationes} air [] destroy`);
     }
 };
 
 export class OrangeGift {
     onPlayerInteract(event){
         const {player, block, dimension, entity} = event;
-        player.runCommand("say Hi");
+        const {x, y, z} = block.center();
+        const blockLocationes = `${x} ${y} ${z}`;
+        player.runCommand("say Surprise!!");
+        dimension.runCommand(`loot spawn ${blockLocationes} loot "gift3"`);
+        dimension.runCommand(`setblock ${blockLocationes} air [] destroy`);
     }
 }
